7e89a0132d43917b5bf4be2531a34654bd390843
Denis <example@gmail.com>
2022-04-06 17:22:02 +0300

    update square

--- index.js
+++ index.js
@@ -1,44 +1,97 @@
-let addHoriz = document.querySelector('.add-horizontal');
-let addVertical = document.querySelector('.add-vertical');
 
-let remHoriz = document.querySelector('.remove-horizontal');
-let remVertical = document.querySelector('.remove-vertical');
+const wrapBox = document.createElement('div');
+const box = document.createElement('div');
 
-let box = document.querySelector('.box');
+const addHoriz = document.createElement('div');
+const remHoriz = document.createElement('div');
 
+const addVertical = document.createElement('div');
+const remVertical = document.createElement('div');
+
+const plus = document.createElement('div');
+const minus = document.createElement('div');
+
+wrapBox.classList.add('wrap-box');
+box.classList.add('box');
+
+addHoriz.classList.add('add-horizontal');
+remHoriz.classList.add('remove-horizontal');
+
+addVertical.classList.add('add-vertical');
+remVertical.classList.add('remove-vertical');
+
+plus.classList.add('plus');
+minus.classList.add('minus');
+
+function start(root) {
+    document.querySelector(root).append(wrapBox);
+
+    wrapBox.append(remVertical);
+    wrapBox.append(remHoriz);
+    wrapBox.append(addHoriz);
+    wrapBox.append(box);
+    wrapBox.append(addVertical);
+
+    remVertical.append(minus.cloneNode());
+    remHoriz.append(minus);
+
+    addHoriz.append(plus.cloneNode());
+    addVertical.append(plus);
+}
+
+start('.wrap');
+
+fillSquere();
+
+function fillSquere() {
+    while (box.firstChild) {
+        box.removeChild(box.lastChild)
+    }
+
+    for (let i = 0; i < Math.floor(box.clientWidth / 52) * Math.floor(box.clientHeight / 52); i++) {
+       let squere = document.createElement('div')
+       squere.classList.add('square')
+       box.append(squere)
+    }
+}
+
+function changeWidth(change) {
+    switch (change) {
+        case "plus":
+            return Math.floor(box.clientWidth) + 54 + 'px'
+        case "minus":
+            return Math.floor(box.clientWidth) - 50 + 'px'
+    }
+}
+
+function changeHeight(change) {
+    switch (change) {
+        case "plus":
+            return Math.floor(box.clientHeight) + 54 + 'px'
+        case "minus":
+            return Math.floor(box.clientHeight) - 50 + 'px'
+    }
+}
 
 addHoriz.addEventListener('click', () => {
-    box.style.width = Math.floor(box.clientWidth) + 54 + 'px'
-    box.innerHTML = '';
-    
-    for (let i = 0; i < Math.floor(box.clientWidth / 52) * Math.floor(box.clientHeight / 52); i++)
-    box.innerHTML += '<div class="squer"></div>';
-})
-
-addVertical.addEventListener('click', () => {
-    box.style.height = Math.floor(box.clientHeight) + 54 + 'px'
-    box.innerHTML = '';
-    
-    for (let i = 0; i < Math.floor(box.clientWidth / 52) * Math.floor(box.clientHeight / 52); i++)
-    box.innerHTML += '<div class="squer"></div>';
-})
-
-remVertical.addEventListener('click', () => {
-    box.style.height = Math.floor(box.clientHeight) - 50 + 'px'
-    box.innerHTML = '';
-    
-    console.log(box.clientWidth)
-    console.log(box.clientHeight)
-    for (let i = 0; i < Math.floor(box.clientWidth / 52) * Math.floor(box.clientHeight / 52); i++)
-    box.innerHTML += '<div class="squer"></div>';
+    box.style.width = changeWidth('plus')
+    fillSquere()
 })
 
 remHoriz.addEventListener('click', () => {
-    box.style.width = Math.floor(box.clientWidth) - 50 + 'px'
-    box.innerHTML = '';
-    
-    console.log(box.clientWidth)
-    console.log(box.clientHeight)
-    for (let i = 0; i < Math.floor(box.clientWidth / 52) * Math.floor(box.clientHeight / 52); i++)
-    box.innerHTML += '<div class="squer"></div>';
+    box.style.width = changeWidth('minus')
+    fillSquere()
 })
+
+addVertical.addEventListener('click', () => {
+    box.style.height = changeHeight('plus')
+    fillSquere()
+})
+
+remVertical.addEventListener('click', () => {
+    box.style.height = changeHeight('minus')
+    fillSquere()
+})
+
+
+
