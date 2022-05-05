656fc5fba31fe2800e3798d4015b3da83f315967
Denis <example@gmail.com>
2022-05-05 20:52:45 +0300

    rebild in class style

--- index.js
+++ index.js
@@ -1,163 +1,167 @@
 
-function initSquare(rows, column, root) {
-    const app = document.querySelector(root)
-
-    let rowAmount = rows;
-    let columnAmount = column; 
-
-    const addVertical = document.createElement('div');
-    addVertical.classList.add('add-vertical_2', 'button');
-    app.appendChild(addVertical);
-
-    const addHoriz = document.createElement('div');
-    addHoriz.classList.add('add-horizontal_2', 'button');
-    document.querySelector(".app").appendChild(addHoriz);
-
-    const remVertical = document.createElement('div');
-    remVertical.classList.add('remove-vertical_2', 'button');
-    app.appendChild(remVertical);
-
-    const remHoriz = document.createElement('div');
-    remHoriz.classList.add('remove-horizontal_2', 'button');
-    app.appendChild(remHoriz);
-
-
-    let table = document.createElement('table');
-    let tbody = document.createElement('tbody');
-
-    const plus = document.createElement('div');
-    const minus = document.createElement('div');
-    plus.classList.add('plus');
-    minus.classList.add('minus');
-
-    addHoriz.appendChild(plus)
-    addVertical.appendChild(plus.cloneNode())
-
-    remVertical.appendChild(minus)
-    remHoriz.appendChild(minus.cloneNode())
-
-    table.appendChild(tbody);
-    app.appendChild(table);
-
-    function hideRemoveButtons() {
-        remHoriz.style.visibility = 'hidden'
-        remVertical.style.visibility = 'hidden'
+class Square {
+    constructor(columns, rows, root) {
+        this.columns = columns;
+        this.rows = rows;
+        this._root = root;
     }
 
-    function showRemoveButtons(horizontal = null, vertical = null) {
-        if (horizontal) {
-            remHoriz.style.visibility = 'visible'
+    _createAddButtons() {
+        const app = document.querySelector(this._root)
+
+        const addVertical = document.createElement('div');
+        addVertical.classList.add('add-vertical_2', 'button');
+        app.append(addVertical);
+
+        const addHoriz = document.createElement('div');
+        addHoriz.classList.add('add-horizontal_2', 'button');
+        app.appendChild(addHoriz);
+
+        const plus = document.createElement('div');
+        plus.classList.add('plus');
+
+        addHoriz.appendChild(plus)
+        addVertical.appendChild(plus.cloneNode())
+
+        let addEventListenerOnAddButtons = () => {
+            addHoriz.addEventListener('click', () => {
+                let row = document.querySelector('tbody').firstElementChild.cloneNode(true)
+                document.querySelector('tbody').appendChild(row)
+            })
+    
+            addVertical.addEventListener('click', () => {
+                let rows = document.querySelectorAll('tr')
+                rows.forEach(item => {
+                    let td = document.createElement('td');
+                    item.appendChild(td)
+                })
+            })
         }
-        if (vertical) {
-            remVertical.style.visibility = 'visible'
-        }
+        addEventListenerOnAddButtons();
     }
 
-    for (let i = 0; i < rowAmount; i++) {
-        let row = document.createElement('tr');
-        tbody.appendChild(row)
-    
-        for (let j = 0; j < columnAmount; j++) {
-            let column = document.createElement('td')
-            row.appendChild(column)
-        }
-    }
+    _createRemoveButtons() {
+        const app = document.querySelector(this._root)
 
+        const remVertical = document.createElement('div');
+        remVertical.classList.add('remove-vertical_2', 'button');
+        app.append(remVertical);
+    
+        const remHoriz = document.createElement('div');
+        remHoriz.classList.add('remove-horizontal_2', 'button');
+        app.append(remHoriz);
 
-    addVertical.addEventListener('click', () => {
-        let rows = document.querySelectorAll('tr')
-        rows.forEach(item => {
-            let td = document.createElement('td');
-            item.appendChild(td)
-        })
-    })
+        const minus = document.createElement('div');
+        minus.classList.add('minus');
+        remVertical.append(minus)
+        remHoriz.append(minus.cloneNode())
 
-    addHoriz.addEventListener('click', () => {
-        let row = document.querySelector('tbody').firstElementChild.cloneNode(true)
-        document.querySelector('tbody').appendChild(row)
-    })
-
-    remHoriz.addEventListener('click', () => {
-        let rows = document.querySelectorAll('tr');
-        let numberToRemove = 0;
-    
-        if (rows.length === 1) {
-            return
-        }
-    
-        rows.forEach((item, i) => {
-            if (remHoriz.offsetTop - item.offsetTop === 2) {
-                numberToRemove = i
-            }
-        })
-        hideRemoveButtons();
-    
-        rows[numberToRemove].remove()
-    })
-
-    remVertical.addEventListener('click', () => {
-        let rows = document.querySelectorAll('tr');
-        let columns = rows[0].querySelectorAll('td')
-    
-        if (columns.length === 1) {
-            return
-        }
-    
-        let numberToRemove = 0;
-    
-        columns.forEach((item, i) => {
-            if (remVertical.offsetLeft - item.offsetLeft === 0) {
-                numberToRemove = i;
-            }
-        })
-    
-        hideRemoveButtons();
-    
-        rows.forEach(item => {
-            item.querySelector(`:nth-child(${numberToRemove + 1})`).remove()
+        let addEventListenerOnRemoveButtons = () => {
+            remHoriz.addEventListener('click', () => {
+                let rows = document.querySelectorAll('tr');
+                let numberToRemove = 0;
             
-        })
-    })
+                if (rows.length === 1) {
+                    return
+                }
+            
+                rows.forEach((item, i) => {
+                    if (remHoriz.offsetTop - item.offsetTop === 2) {
+                        numberToRemove = i
+                    }
+                })
+                this._hideRemoveButtons();
+            
+                rows[numberToRemove].remove()
+            })
 
-    app.addEventListener('mouseover', (e) => {
-
-
-        if (e.target && e.target.tagName === 'TD') {
-            // showRemoveButtons takes 2 argument first: "horizontalBtn" second: "verticalBtn"
-            showRemoveButtons(true);
-            remHoriz.style.top = e.target.offsetTop + 'px'
-    
-            showRemoveButtons(false, true)
-            remVertical.style.left = e.target.offsetLeft + 'px'
-        } else {
-            showRemoveButtons(true, true)
+            remVertical.addEventListener('click', () => {
+                let rows = document.querySelectorAll('tr');
+                let columns = rows[0].querySelectorAll('td')
+            
+                if (columns.length === 1) {
+                    return
+                }
+            
+                let numberToRemove = 0;
+            
+                columns.forEach((item, i) => {
+                    if (remVertical.offsetLeft - item.offsetLeft === 0) {
+                        numberToRemove = i;
+                    }
+                })
+            
+                this._hideRemoveButtons();
+            
+                rows.forEach(item => {
+                    item.querySelector(`:nth-child(${numberToRemove + 1})`).remove()
+                    
+                })
+            })
         }
-    })
+        addEventListenerOnRemoveButtons();
+    }
+
+
+
+    init = () => {
+        const app = document.querySelector(this._root)
+        
+        this._createAddButtons()
+        this._createRemoveButtons()
+        
+        let table = document.createElement('table');
+        let tbody = document.createElement('tbody');
     
-    app.addEventListener('mouseout', () => {
-        hideRemoveButtons();
-    })
+        table.append(tbody);
+        app.append(table);
+
+        for (let i = 0; i < this.rows; i++) {
+            let row = document.createElement('tr');
+            tbody.append(row)
+        
+            for (let j = 0; j < this.columns; j++) {
+                let column = document.createElement('td')
+                row.append(column)
+            }
+        }
+    
+        app.addEventListener('mouseover', (e) => {
+            if (e.target && e.target.tagName === 'TD') {
+                // showRemoveButtons takes 2 argument first: "horizontalBtn" second: "verticalBtn"
+                console.log()
+                this._showRemoveButtons(true);
+                document.querySelector('.remove-horizontal_2').style.top = e.target.offsetTop + 'px'
+        
+                this._showRemoveButtons(false, true)
+                document.querySelector('.remove-vertical_2').style.left = e.target.offsetLeft + 'px'
+            } else {
+                this._showRemoveButtons(true, true)
+            }
+        })
+        
+        app.addEventListener('mouseout', () => {
+            this._hideRemoveButtons();
+        })
+    
+    }
+
+    _hideRemoveButtons() {
+        document.querySelector('.remove-horizontal_2').style.visibility = 'hidden'
+        document.querySelector('.remove-vertical_2').style.visibility = 'hidden'
+    }
+
+    _showRemoveButtons(horizontal = null, vertical = null) {
+        if (horizontal && document.querySelectorAll('tr').length !== 1) {
+            document.querySelector('.remove-horizontal_2').style.visibility = 'visible'
+        }
+        if (vertical && (document.querySelectorAll('td').length / document.querySelectorAll('tr').length) !== 1) {
+            document.querySelector('.remove-vertical_2').style.visibility = 'visible'
+        }
+    }
 
 }
 
-initSquare(4, 4, ".app")
+const square = new Square(4, 4, '.app');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+square.init();
